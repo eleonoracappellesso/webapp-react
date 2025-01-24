88326e6d659c19ee660db19223a8f05f91a04a88
eleonoracappellesso <cappellessoeleonora@gmail.com>
2025-01-24 14:43:13 +0100

    created homePage and DetailPage

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,20 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom"
+
+import HomePage from "./pages/HomePage"
+import MovieDetailPage from "./pages/MovieDetailPage"
+
 export default function App() {
-  return <h1>homepage</h1>
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/movies">
+          <Route index Component={HomePage} />
+          <Route path=":id" Component={MovieDetailPage} />
+        </Route>
+
+      </Routes>
+    </BrowserRouter>
+  )
 }
 
 
